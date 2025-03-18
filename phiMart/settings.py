e6799cb59aafbc217240636fcc21518e96ba6d00
zishan344 <zishanahmed344@gmail.com>
2025-03-18 14:30:21 +0600

    Remove unused image files and clean up settings.py for better clarity

--- phiMart/settings.py
+++ phiMart/settings.py
@@ -7,9 +7,6 @@ import cloudinary
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-^6it)f=-=+rc56k(&oeua9)reb_@hl+n8vlkdv&ljq7-*zdfwg'
 
@@ -76,16 +73,6 @@ WSGI_APPLICATION = 'phiMart.wsgi.app'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-""" DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-"""
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -145,8 +132,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
