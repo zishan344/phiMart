5d8b284a43dff7c2d262450e15b701d695c3a0f4
zishan344 <zishanahmed344@gmail.com>
2025-03-26 11:38:07 +0600

    Refactor settings.py for improved configuration; update .gitignore to exclude db.sqlite3; remove requirements.txt; add email backend settings; adjust Vercel configuration.

--- phiMart/settings.py
+++ phiMart/settings.py
@@ -2,17 +2,21 @@ from pathlib import Path
 from datetime import timedelta
 from decouple import config
 import cloudinary
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^6it)f=-=+rc56k(&oeua9)reb_@hl+n8vlkdv&ljq7-*zdfwg'
+SECRET_KEY = 'django-insecure-_-sw+995f4t48rwyxucty93nmor3r&u0(secce*$8+36=xcv3-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
+ALLOWED_HOSTS = [".vercel.app", '127.0.0.1']
 AUTH_USER_MODEL = 'users.User'
 
 # Application definition
@@ -25,17 +29,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
+    'django_filters',
+    "corsheaders",
     'rest_framework',
     'djoser',
-    'django_filters',
     'api',
-    'users',
     'product',
-    'order'
+    'users',
+    'order',
+    "debug_toolbar",
 ]
 
-# Remove debug_toolbar from production middleware
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -46,24 +54,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# Debug toolbar and swagger only in development
-if DEBUG:
-    INSTALLED_APPS += ['drf_yasg', 'debug_toolbar']
-    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
-SWAGGER_SETTINGS = {
-    'SECURITY_DEFINITIONS': {
-        'Bearer': {
-            'type': 'apiKey',
-            'name': 'Authorization',
-            'in': 'header',
-            'description':'Enter your jwt token in the format: `JWT <your_token>`'
-        }
-    }
-}
-
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
 ROOT_URLCONF = 'phiMart.urls'
 
 TEMPLATES = [
@@ -84,8 +74,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'phiMart.wsgi.app'
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5174',
+]
 
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 # Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -93,15 +99,11 @@ DATABASES = {
         'USER': config('user'),
         'PASSWORD': config('password'),
         'HOST': config('host'),
-        'PORT': config('port'),
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
+        'PORT': config('port')
     }
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -123,12 +125,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 # Configuration for cloudinary storage
-cloudinary.config( 
-    cloud_name = config('cludname'),
-    api_key = config('cloudinary_api_key'),
-    api_secret = config('cloudinary_api_secret'),
+cloudinary.config(
+    cloud_name=config('cloud_name'),
+    api_key=config('cloudinary_api_key'),
+    api_secret=config('api_secret'),
     secure=True
 )
+# Media storage setting
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 LANGUAGE_CODE = 'en-us'
@@ -145,10 +148,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_FILES_DIR = BASE_DIR / 'static'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
@@ -156,9 +161,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': [
-    'rest_framework.permissions.IsAuthenticated',
-]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ]
 }
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
@@ -166,9 +171,30 @@ SIMPLE_JWT = {
 }
 
 DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': 'activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
     'SERIALIZERS': {
         'user_create': 'users.serializers.UserCreateSerializer',
         'current_user': 'users.serializers.UserSerializer'
     },
 }
 
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+            'description': 'Enter your JWT token in the format: `JWT <your_token>`'
+        }
+    }
+}
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
